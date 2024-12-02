92f7cde3673343ea376ce83add07bdc1c3a51ccf
seokmin <msm0748@gmail.com>
2024-12-03 01:38:27 +0900

    comment: 환경설정에 주석 추가

--- next.config.mjs
+++ next.config.mjs
@@ -1,43 +1,46 @@
-import {withSentryConfig} from '@sentry/nextjs';
+import { withSentryConfig } from '@sentry/nextjs';
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
 
 export default withSentryConfig(nextConfig, {
-// For all available options, see:
-// https://github.com/getsentry/sentry-webpack-plugin#options
+  // 사용 가능한 모든 옵션은 다음을 참조하세요:
+  // https://github.com/getsentry/sentry-webpack-plugin#options
 
-org: "playidealab-la",
-project: "sentry-study",
+  org: 'playidealab-la',
+  project: 'sentry-study',
 
-// Only print logs for uploading source maps in CI
-silent: !process.env.CI,
+  // CI에서 소스 맵 업로드 로그만 인쇄
+  silent: !process.env.CI,
 
-// For all available options, see:
-// https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/
+  // 사용 가능한 모든 옵션은 다음을 참조하세요:
+  // https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/
 
-// Upload a larger set of source maps for prettier stack traces (increases build time)
-widenClientFileUpload: true,
+  // 더 예쁜 스택 추적을 위해 더 많은 소스 맵을 업로드합니다 (빌드 시간이 증가함)
+  widenClientFileUpload: true,
 
-// Automatically annotate React components to show their full name in breadcrumbs and session replay
-reactComponentAnnotation: {
-enabled: true,
-},
+  // React 컴포넌트를 자동으로 주석 처리하여 breadcrumbs 및 세션 재생에서 전체 이름을 표시합니다
+  reactComponentAnnotation: {
+    enabled: true,
+  },
 
-// Route browser requests to Sentry through a Next.js rewrite to circumvent ad-blockers.
-// This can increase your server load as well as your hosting bill.
-// Note: Check that the configured route will not match with your Next.js middleware, otherwise reporting of client-
-// side errors will fail.
-tunnelRoute: "/monitoring",
+  // tunnelRoute 옵션의 목적은 ad-blockers(크롬 웹스토어 광고 차단기) 등으로 인해 Sentry 데이터가 차단되지 않도록 우회 경로를 제공하는 데 있음
+  // 이러한 서비스의 요청은 사용자 데이터를 수집한다고 오해될 수 있으며, 기본적으로 추적(tracking) 도구로 간주되어 차단 리스트에 올라가게 된다고 함.
 
-// Hides source maps from generated client bundles
-hideSourceMaps: true,
+  // tunnelRoute를 통해 엔드포인트를 자신의 서버로 리라이트하면 이러한 차단을 피할 수 있음
+  // next 서버에서 sentry 서버로 데이터를 전달
+  // 서버 부하 주의 !!
+  // 주석하면 클라이언트에서 sentry 서버로 다이렉트 전송
+  //   tunnelRoute: '/monitoring',
 
-// Automatically tree-shake Sentry logger statements to reduce bundle size
-disableLogger: true,
+  // 생성된 클라이언트 번들에서 소스 맵을 숨깁니다
+  hideSourceMaps: true,
 
-// Enables automatic instrumentation of Vercel Cron Monitors. (Does not yet work with App Router route handlers.)
-// See the following for more information:
-// https://docs.sentry.io/product/crons/
-// https://vercel.com/docs/cron-jobs
-automaticVercelMonitors: true,
+  // 번들 크기를 줄이기 위해 Sentry 로거 문장을 자동으로 트리 쉐이킹합니다
+  disableLogger: true,
+
+  // Vercel Cron 모니터의 자동 계측을 활성화합니다. (현재 App Router 경로 핸들러와는 작동하지 않음)
+  // 자세한 내용은 다음을 참조하세요:
+  // https://docs.sentry.io/product/crons/
+  // https://vercel.com/docs/cron-jobs
+  automaticVercelMonitors: true,
 });
--- sentry.client.config.ts
+++ sentry.client.config.ts
@@ -8,16 +8,24 @@ Sentry.init({
   dsn: 'https://97e90b594b3c3fa2c6e1ced249edac55@o4508369430642688.ingest.us.sentry.io/4508398533410816',
 
   // 선택적 통합을 추가하여 추가 기능을 사용할 수 있습니다
-  integrations: [Sentry.replayIntegration()],
+  integrations: [
+    // Sentry는 소프트웨어 성능을 추적하고 처리량 및 대기 시간과 같은 메트릭을 측정하고 여러 시스템에서 오류가 미치는 영향을 표시
+    // default는 포함되지 않음
+    Sentry.browserTracingIntegration(),
+    // 사용자의 브라우저에서 발생하는 일을 비디오처럼 재현
+    Sentry.replayIntegration(),
+  ],
 
-  // 추적이 샘플링되는 가능성을 정의합니다. 이 값을 프로덕션에서 조정하거나, 더 큰 제어를 위해 tracesSampler를 사용할 수 있습니다.
+  // 샘플링 비율이 0.1(10%)이라는 것은 10번 중 1번만 기록된다는 의미
+
+  // 성능 트랜잭션 샘플링
   tracesSampleRate: 1,
 
-  // Replay 이벤트가 샘플링되는 가능성을 정의합니다.
-  // 이 설정은 샘플 비율을 10%로 설정합니다. 개발 중에는 100%로 설정하고 프로덕션에서는 더 낮은 비율로 샘플링할 수 있습니다.
+  // 사용자 세션 Replay 샘플링 (오류가 발생하지 않아도 사용자 세션을 수집)
+  // 애플리케이션에서 사용자가 어떻게 상호작용했는지를 분석할 수 있음.
   replaysSessionSampleRate: 0.1,
 
-  // 오류가 발생할 때 Replay 이벤트가 샘플링되는 가능성을 정의합니다.
+  // 오류 발생 시 사용자 세션 Replay 샘플링
   replaysOnErrorSampleRate: 1.0,
 
   // 이 옵션을 true로 설정하면 Sentry를 설정하는 동안 유용한 정보가 콘솔에 출력됩니다.
